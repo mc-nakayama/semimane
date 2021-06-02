ddba21a434cff43c71fb45ad4d159f88bb17da3f
mc-nakayama <michitoshi.nakayama@gmail.com>
2021-06-02 16:10:40 +0900

    seminar_controller
    # what
    seminarsにおける
    index、new、createアクションを設定。
    ルーティングの設定。

---
+++ app/controllers/seminars_controller.rb
@@ -0,0 +1,22 @@
+class SeminarsController < ApplicationController
+  def index
+    @seminars = Seminar.all
+  end
+
+  def new
+    @seminar = Seminar.new
+  end
+
+  def create
+    @seminar = Seminar.new
+    if @seminar.save(seminar_params)
+      redirect_to root_path, notice: 'グループを作成しました'      
+    else
+      render :new
+    end
+  end
+  
+  end
+  
+  
+end
