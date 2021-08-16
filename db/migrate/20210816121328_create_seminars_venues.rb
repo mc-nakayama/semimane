c17f9bba10d69c662db7cbace8837d7bade2ed0e
mc-nakayama <naka.demy.school@gmail.com>
2021-08-16 22:04:34 +0900

    association設定

--- db/migrate/20210816121328_create_seminars_venues.rb
+++ db/migrate/20210816121328_create_seminars_venues.rb
@@ -1,7 +1,8 @@
 class CreateSeminarsVenues < ActiveRecord::Migration[6.0]
   def change
     create_table :seminars_venues do |t|
-
+      t.references :seminar,foreign_key:true
+      t.references :venue,foreign_key:true
       t.timestamps
     end
   end
