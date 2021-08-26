a9c56644f9e8449fea0b8636f742f2801baca551
mc-nakayama <naka.demy.school@gmail.com>
2021-08-26 10:37:58 +0900

    show_controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,5 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedule, only:[:show]
   def new
     @schedule = Schedule.new
   end
@@ -7,9 +8,16 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.create(schedule_params)
     redirect_to root_path, notice: 'を登録しました'
   end
+
+  def show
+  end
   
   private
   def schedule_params
     params.require(:schedule).permit(:schedule_date,:start_time,:stop_time,:seminar_id,:venue_id,:reservation,:participation,:memo)
+  end
+  
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
   end
 end
